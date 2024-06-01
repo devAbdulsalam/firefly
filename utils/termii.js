d9257e0cbbd0c4f356058cec47e773b5cfc00bb4
devabdulsalam <ammuftau74@gmail.com>
2024-06-01 13:04:43 +0100

    first commit

--- utils/termii.js
+++ utils/termii.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const apiKey = process.env.TERMII_API_KEY;
 const apiUrl = process.env.TERMII_API_URL;
+const senderId = process.env.TERMII_SENDER_ID;
 
 export const sendSMS = async (message, phone) => {
 	try {
@@ -12,18 +13,19 @@ export const sendSMS = async (message, phone) => {
 		// const { data: senderIds } = await axios.get(
 		// 	`${apiUrl}/sender-id?api_key=${apiKey}`
 		// );
-		const senderIdResponse = await axios.post(`${apiUrl}/sender-id/request`, {
-			apiKey,
-			serder_id: 'FireFly',
-			company: 'firefly corps',
-			useCcse: 'your firefly id is 1234',
-		});
-		console.log('senderIdResponse', senderIdResponse);
+		// const senderIdResponse = await axios.post(`${apiUrl}/sender-id/request`, {
+		// 	apiKey,
+		// 	serder_id: senderId,
+		// 	company: 'firefly corps',
+		// 	useCcse: 'your firefly id is 1234',
+		// });
+		// console.log('senderIdResponse', senderIdResponse);
 
 		const response = await axios.post(`${apiUrl}/sms/send`, {
 			from: 'talert',
 			sms: 'Hi there, testing Termii',
 			// sms: message,
+			serder_id: senderId,
 			to: phone,
 			type: 'plain',
 			api_key: apiKey,
